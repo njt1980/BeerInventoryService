feaa734d4a968ab33d9e3f7ce6c5fdc902d443ec
john <thomas.nimel@gmail.com>
2021-06-30 02:12:52 +0530

    Commiting working code - 6_30

--- src/main/java/com/nimel/mymicroservices/inventoryservice/listeners/AllocationRequestListener.java
+++ src/main/java/com/nimel/mymicroservices/inventoryservice/listeners/AllocationRequestListener.java
@@ -1,34 +1,52 @@
 package com.nimel.mymicroservices.inventoryservice.listeners;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
 
 import com.nimel.mymicroservices.common.dtos.AllocateOrderRequest;
-import com.nimel.mymicroservices.common.events.AllocateOrderResult;
+import com.nimel.mymicroservices.common.dtos.AllocateOrderResult;
 import com.nimel.mymicroservices.inventoryservice.config.JmsConfig;
 import com.nimel.mymicroservices.inventoryservice.services.AllocationService;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Component
+@AllArgsConstructor
+@NoArgsConstructor
 public class AllocationRequestListener {
 	
+	@Autowired
 	private AllocationService allocationService;
+	@Autowired
 	private JmsTemplate jmsTemplate;
 	
 	@JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
 	public void listenForAllocationRequests(AllocateOrderRequest request) {
 		
+		System.out.println("In Inventory service - Listening to ALLOCATE_ORDER_QUEUE" + request.getBeerOrderDto());
+		System.out.println("Request id in Allocate Order queue :" + request.getBeerOrderDto().getId());
 		AllocateOrderResult.AllocateOrderResultBuilder builder = AllocateOrderResult.builder();
 		builder.beerOrderDto(request.getBeerOrderDto());
+		builder.allocationError(false);
+		builder.pendingInventory(false);
 		
 		try {
+			System.out.println("Going to allocateOrder");
 			Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDto());
+			System.out.println("Allocation result for " + request.getBeerOrderDto().getId() + "is " + allocationResult);
 			if(allocationResult) {
+				System.out.println("In Inventory service - Inventory Not Pending");
 				builder.pendingInventory(false);
 			}else {
+				System.out.println("In Inventory service - Inventory Pending");
 				builder.pendingInventory(true);
 			}
 		}catch(Exception e) {
 			
-			System.out.println("Allocation error");
+			System.out.println("Allocation error" + e);
 			builder.allocationError(true);
 			
 		}
--- src/main/java/com/nimel/mymicroservices/inventoryservice/services/AllocationServiceImpl.java
+++ src/main/java/com/nimel/mymicroservices/inventoryservice/services/AllocationServiceImpl.java
@@ -3,6 +3,8 @@ package com.nimel.mymicroservices.inventoryservice.services;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.AbstractAuditable_;
 import org.springframework.stereotype.Service;
 
 import com.nimel.mymicroservices.common.dtos.BeerOrderDto;
@@ -13,6 +15,7 @@ import com.nimel.mymicroservices.inventoryservice.respository.InventoryRepositor
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import lombok.With;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -21,15 +24,21 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AllocationServiceImpl implements AllocationService {
 	
+	@Autowired
 	private InventoryRepository inventoryRepository;
 	@Override
     public Boolean allocateOrder(BeerOrderDto beerOrderDto) {
         log.debug("Allocating OrderId: " + beerOrderDto.getId());
+        System.out.println("Inventory Repo: is " + inventoryRepository);
+        System.out.println("OrderDto is : " + beerOrderDto);
+        System.out.println("Allocating OrderId: " + beerOrderDto.getId());
         AtomicInteger totalOrdered = new AtomicInteger();
         AtomicInteger totalAllocated = new AtomicInteger();
         beerOrderDto.getBeerOrderLines().forEach(beerOrderLine -> {
             if ((((beerOrderLine.getOrderQuantity() != null ? beerOrderLine.getOrderQuantity() : 0)
                     - (beerOrderLine.getQuantityAllocated() != null ? beerOrderLine.getQuantityAllocated() : 0)) > 0)) {
+            	System.out.println("Trying to allocate for beerorderlines!");
+            	System.out.println("BeerOrderLine is " + beerOrderLine);
                 allocateBeerOrderLine(beerOrderLine);
             }
             totalOrdered.set(totalOrdered.get() + beerOrderLine.getOrderQuantity());
@@ -37,18 +46,26 @@ public class AllocationServiceImpl implements AllocationService {
         });
 
         log.debug("Total Ordered: " + totalOrdered.get() + " Total Allocated: " + totalAllocated.get());
-
+        System.out.println("In allocate order for beer id " + beerOrderDto.getId() + "Total ordered " + totalOrdered.get());
+        System.out.println("In allocate order for beer id " + beerOrderDto.getId() + "Total Allocated " + totalAllocated.get());
         return totalOrdered.get() == totalAllocated.get();
     }
 
     private void allocateBeerOrderLine(BeerOrderLineDto beerOrderLine) {
-
+    	
+    	System.out.println("Beer Line Upc : for finding Inventory" + beerOrderLine.getUpc());
+//    	System.out.println("Inventory for Beer Line Upc : " + inventoryRepository.findByUpc(beerOrderLine.getUpc()));
+    	System.out.println(inventoryRepository.findAllByUpc(beerOrderLine.getUpc()));
     	List<Inventory> beerInventoryList = inventoryRepository.findAllByUpc(beerOrderLine.getUpc());
+    	System.out.println("Beer Inventory List for upc " + beerOrderLine.getUpc() + " is " + beerInventoryList);
 
         beerInventoryList.forEach(beerInventory -> {
             int inventory = (beerInventory.getQuantityOnHand() == null) ? 0 : beerInventory.getQuantityOnHand();
+            System.out.println("**Quantiy on Hand for upc " + beerOrderLine.getUpc() + "is" + inventory);
             int orderQty = (beerOrderLine.getOrderQuantity() == null) ? 0 : beerOrderLine.getOrderQuantity();
+            System.out.println("**Quantiy on Hand for upc " + beerOrderLine.getUpc() + "is" + orderQty);
             int allocatedQty = (beerOrderLine.getQuantityAllocated() == null) ? 0 : beerOrderLine.getQuantityAllocated();
+            System.out.println("**Alloc Quantiy for upc " + beerOrderLine.getUpc() + "is" + allocatedQty);
             int qtyToAllocate = orderQty - allocatedQty;
 
             if (inventory >= qtyToAllocate) { // full allocation
